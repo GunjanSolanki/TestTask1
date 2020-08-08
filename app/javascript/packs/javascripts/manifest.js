adf942eae169d2eca5c9fe63b5bc30d23111ec32
Gunjan Solanki <gsolanki@techracers.io>
2020-08-09 00:24:53 +0530

    Add Assets & Designs

---
+++ app/javascript/packs/javascripts/manifest.js
@@ -0,0 +1,6 @@
+import './initializers/jquery.min';
+import './initializers/rails.js';
+import './bootstrap/bootstrap.min.js';
+import './nested-forms/addFields.js';
+import './nested-forms/removeFields.js';
+import './daterangepicker_initializer.js';
